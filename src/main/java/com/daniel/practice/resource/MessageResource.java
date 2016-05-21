8eaa04739ddb3bfefc60072f2e69712da19bcfd5
wheezymustafa <danielm8686@gmail.com>
2016-05-21 11:16:44 -0500

    trying to fix build

--- src/main/java/com/daniel/practice/resource/MessageResource.java
+++ src/main/java/com/daniel/practice/resource/MessageResource.java
@@ -5,10 +5,8 @@ import com.daniel.practice.service.MessageService;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.util.List;
 import java.util.Map;
 
 
@@ -16,20 +14,16 @@ import java.util.Map;
 @Path("/messages")
 public class MessageResource {
 
-        MessageService messageService = new MessageService();
+    MessageService messageService = new MessageService();
 
-/*        @GET
-        @Produces(MediaType.APPLICATION_XML)
-        public Map<Long, Message> getMessages() {
-            return messageService.getAllMessages();
-        }
-        //System.out.println("Hello");
+    @GET
+    @Produces(MediaType.APPLICATION_XML)
+    public Map<Long,Message> getAllMessages(){
 
-        @GET
-        @Path("/{messageId}")
-        @Produces(MediaType.APPLICATION_XML)
-        public Message getMessage(@PathParam("messageId") Long id){
-                return messageService.getMessage(id);
-        }*/
+        return messageService.getAllMessages();
+
+    }
+
+
 
 }
